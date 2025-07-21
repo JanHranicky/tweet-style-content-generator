7aab564c33951a09eb45841037fd3845fdf65863
JanHranicky <jan.hranicky@seznam.cz>
2025-07-21 13:30:29 +0200

    moved ragWebSearchInput into searchQuery function

--- src/rag-web-search.ts
+++ src/rag-web-search.ts
@@ -1,35 +1,36 @@
 import { log } from 'apify';
-import type { ActorStartOptions} from 'apify-client';
+import type { ActorStartOptions } from 'apify-client';
 import { ApifyClient } from 'apify-client';
 
 import type { Input, RagWebSearchInput, webContent } from './types.js';
 
 const { APIFY_TOKEN } = process.env;
 
-const ragWebSearchInput: RagWebSearchInput  = {
-        "query": "",
-        "proxyConfiguration": {
-            "useApifyProxy": true
-        },
-        "maxResults": 3,
-        "removeElementsCssSelector": `nav, footer, script, style, noscript, svg, img[src^='data:'], a[href],
-            [role="alert"],
-            [role="banner"],
-            [role="dialog"],
-            [role="alertdialog"],
-            [role="region"][aria-label*="skip" i],
-            [aria-modal="true"]`,
-        "htmlTransformer": "extract only main content of the pages, no links or navigation. Remove newlines, whitecharacters, any style characters. Compress the content for LLM",
-    };
 
 /**
  * runs apify's rag-web-browser actor to crawl the web for given query. Returns an array of queried data
  * @param input tweet-style-content-generator actor's input
  * @param query user defined query, either single url or a phrase
  * @returns content array of crawled websites
- */
+*/
 export async function searchQuery(input: Input, query: string): Promise<webContent[]> {
     try {
+        const ragWebSearchInput: RagWebSearchInput = {
+            "query": query, // save user defined query to actors input
+            "proxyConfiguration": {
+                "useApifyProxy": true
+            },
+            "maxResults": input.maxResults,  // save user defined query to actors input
+            "removeElementsCssSelector": `nav, footer, script, style, noscript, svg, img[src^='data:'], a[href],
+                    [role="alert"],
+                    [role="banner"],
+                    [role="dialog"],
+                    [role="alertdialog"],
+                    [role="region"][aria-label*="skip" i],
+                    [aria-modal="true"]`,
+            "htmlTransformer": "extract only main content of the pages, no links or navigation. Remove newlines, whitecharacters, any style characters. Compress the content for LLM",
+        };
+
         const client = new ApifyClient({
             token: APIFY_TOKEN,
         });
@@ -38,14 +39,12 @@ export async function searchQuery(input: Input, query: string): Promise<webConte
             memory: input.scrapperMemoryLimit
         }
 
-        ragWebSearchInput.query = query; // save user defined query to actors input
-        ragWebSearchInput.maxResults = input.maxResults; // save user defined query to actors input
 
         const run = await client.actor("apify/rag-web-browser").call(ragWebSearchInput, ragWebSearchOptions);
 
         const { items } = await client.dataset(run.defaultDatasetId).listItems();
 
-        return items.map((item) => ({metadata: item.metadata, markdown: item.markdown} as webContent));
+        return items.map((item) => ({ metadata: item.metadata, markdown: item.markdown } as webContent));
     } catch (error) {
         log.error(`Error while searching query: ${error}`);
         return [];
